09e5d68c8e2f4eeee185c76f37cb540be98c391f
Joab-David-Johanan <joabdavid96@gmail.com>
2025-06-09 16:54:36 +0200

    Finding the product of two numbers

---
+++ product/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    cout<<"Enter the two numbers: "<<endl;
+
+    // storing the user inputs in two variables
+    int var1=0,var2=0;
+    cin>>var1>>var2;
+
+    cout<<"The product of "<<var1<<" and "<<var2<<" is "<<var1*var2<<endl;
+    
+    return 0;
+}
